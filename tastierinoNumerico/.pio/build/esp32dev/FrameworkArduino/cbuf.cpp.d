3dbef3d50f7d5d83c33579bca8e03ac6eeb4a614
Michele <michele.bellucci07@iisgubbio.edu.it>
2024-06-18 19:31:10 +0200

    rimossi file superflui

--- tastierinoNumerico/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/belmi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/belmi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
